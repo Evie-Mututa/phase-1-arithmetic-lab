6d238c5e50c206ff1ceeb0d5d8e4a7f20e4b678f
Evie-Mututa <evemututa29@gmail.com>
2023-09-27 07:03:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+if (true) {
+ let num1 = 2;
+ let num2 =31;
+ let multiply = num1 * num2
+ if (multiply === 62)
+}
+
+if (true) {
+ let randomDecimal = Math.random(0); 
+ let random = Math.floor(randomDecimal * Number.MAX_SAFE_INTEGER) + 1;
+}
+
+if (true) {
+ let num3 = 18;
+ let num4 = 6;
+ let mod = (num3 % num4);
+}
+
+if (true) {
+ let numbers = [12, 15, 20, 10, 8];
+ let max = Math.max(20)
+
+}
+
+
+
